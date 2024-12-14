89406ed656a2566af2370e10295728066b901002
Felihum <121069824+Felihum@users.noreply.github.com>
2024-12-14 00:03:30 -0300

    Files adjusted

--- src/main.jsx
+++ src/main.jsx
@@ -2,13 +2,9 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import { RoutesComponent } from './routes/RoutesComponent'
-import App from './App.jsx'
-import { RouteComponent } from './routes/RouteComponent.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <RoutesComponent />
-  </StrictMode>
-    <RouteComponent />
   </StrictMode>
 )
