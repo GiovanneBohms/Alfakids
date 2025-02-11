4631d3226eb9eba758cffbf0655b030ee18593cf
Felihum <121069824+Felihum@users.noreply.github.com>
2025-02-11 18:59:43 -0300

    update: AI model updated.

--- src/services/ChatbotService.js
+++ src/services/ChatbotService.js
@@ -6,13 +6,11 @@ const modelURL = "http://localhost:11434/api/generate"
 
 export async function sendMessage(message){
     let data = {};
-    data.model = "llama3.2:3b"
+    data.model = "AlfaCopilot"
     data.prompt = message
     data.stream = false
 
-    console.log(data)
     const response = await api.post(modelURL, data)
 
-    console.log(response.data)
     return response.data.response
 }
