f48e9018498f7fba0faf34778602a8368f4a1895
Felihum <felipemedeirosinfo@gmail.com>
2025-02-13 14:14:57 -0300

    component: Message Bubble component added.

---
+++ src/components/MessageBubble/index.jsx
@@ -0,0 +1,36 @@
+import "./index.css"
+
+export function MessageBubble({msg, type}){
+
+    //Date Format
+    const formatDate = (timestamp) => {
+        const today = new Date();
+        const isToday = timestamp.toDateString() === today.toDateString();
+        const time = timestamp.toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' });
+        return isToday ? `Hoje às ${time}` : timestamp.toLocaleString();
+    }
+
+    if(type === "request"){
+        return(
+            <div className="messageBubbleContainer requestContainer">
+                <div className="messageBubble request">
+                    <p className="messageBubbleText">{msg.text}</p>
+                    {/* <span className="timestamp">{formatDate(msg.timestamp)}</span> */}
+                </div>
+            </div>
+        )
+    }
+    
+    if(type === "response"){
+        return(
+            <div className="messageBubbleContainer responseContainer">
+                <div className="messageBubble response">
+                    <p className="messageBubbleText">{msg.text}</p>
+                    {/* <span className="timestamp">{formatDate(msg.timestamp)}</span> */}
+                </div>
+            </div>
+        )
+    }
+
+    return null
+}
